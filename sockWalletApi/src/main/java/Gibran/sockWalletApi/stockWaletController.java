4667cffd54472c460527725277447415ed1b0886
Gibran Rubinger <gibranrubinger@gmail.com>
2020-06-23 23:34:28 +0100

    Rest controller pu

--- sockWalletApi/src/main/java/Gibran/sockWalletApi/stockWaletController.java
+++ sockWalletApi/src/main/java/Gibran/sockWalletApi/stockWaletController.java
@@ -4,13 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class sotckWaletController {
-	
+public class stockWaletController {
+
 	@GetMapping("test")
 	public String test() {
 		return "that is ok";
 	}
-	
-	
 
 }
