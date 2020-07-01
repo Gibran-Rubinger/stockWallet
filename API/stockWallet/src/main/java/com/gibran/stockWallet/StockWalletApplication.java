019a8fda6e1161ddac93c2b0202cc6d21c707b79
Gibran Rubinger <gibranrubinger@gmail.com>
2020-07-01 19:41:43 +0100

    New restApi
    New RestApi

---
+++ API/stockWallet/src/main/java/com/gibran/stockWallet/StockWalletApplication.java
@@ -0,0 +1,13 @@
+package com.gibran.stockWallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockWalletApplication.class, args);
+	}
+
+}
---
+++ API/stockWallet/src/test/java/com/gibran/stockWallet/StockWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gibran.stockWallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
