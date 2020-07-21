63bfaf0642085b091ad453573e23486c3b500e00
Gibran Rubinger <gibranrubinger@gmail.com>
2020-07-21 15:01:16 +0100

    fix bug with overflow
    fixed

--- frontEnd/js/script.js
+++ frontEnd/js/script.js
@@ -7,30 +7,10 @@ menuToggle.addEventListener('click', () => {
     nav.classList.toggle('active')
     bars.classList.toggle('change')
 });
-/*script to change the home photo*/
-const changePhoto = document.querySelector('.bg-parlex');
-const oldPhoto = document.querySelector('.bg-parlex-new');
 
 
 
 
-// Detect if scrollbar is present
-var scrollbar = (some expression) ? true : false;
-if (scrollbar) {
- // Add padding
-}
-$('body').addClass('modal-open');
 
-
-
-/*over quote
-function noiseOut()
-{
-    document.getElementById('quoteNoise').setAttribute('../..;
-}
-function noiseIn()
-{
-    document.getElementById('quoteNoise').src = "../..;
-}*/
 
             
